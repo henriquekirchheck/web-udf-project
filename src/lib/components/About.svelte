e0155a04bd5f16e0f4d7eb11d2cedf104b41a972
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2025-06-03 16:50:38 -0300

    Added about

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -1,20 +1,33 @@
-<div class="mt-2 flex w-full items-center justify-center" id="sobre">
+<script lang="ts">
+  import * as Card from "./ui/card";
+</script>
+
+<div class="mt-2 flex w-full items-center justify-center px-4" id="sobre">
   <div class="prose dark:prose-invert">
-    <h1 class="text-center">Bem vindo ao Cucas do Sul!</h1>
+    <h1 class="wra text-center">Bem vindo ao<wbr /> Cucas&nbsp;do&nbsp;Sul!</h1>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem consequuntur voluptatem
       temporibus unde, expedita atque dignissimos ea odio quo. Repellat quasi aperiam illum sapiente
       mollitia?
     </p>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem consequuntur voluptatem
-      temporibus unde, expedita atque dignissimos ea odio quo. Repellat quasi aperiam illum sapiente
-      mollitia?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic dignissimos quidem pariatur,
+      illum, quae non atque, officia dolores ea dolorem aspernatur magnam nobis commodi? Molestiae
+      officiis soluta, ab beatae delectus adipisci perspiciatis!
     </p>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem consequuntur voluptatem
-      temporibus unde, expedita atque dignissimos ea odio quo. Repellat quasi aperiam illum sapiente
-      mollitia?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic dignissimos quidem pariatur,
+      illum, quae non atque, officia dolores ea dolorem aspernatur magnam nobis commodi? Molestiae
+      officiis soluta, ab beatae delectus adipisci perspiciatis!
+    </p>
+    <Card.Root>
+      <Card.Content>Image</Card.Content>
+    </Card.Root>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed aliquid error dolorum at ducimus
+      commodi ab veniam sapiente eos quibusdam necessitatibus saepe est distinctio dignissimos
+      nesciunt voluptatum mollitia, accusantium facere laboriosam nihil libero itaque deleniti et?
+      Commodi aliquid quo animi incidunt minus! Ullam, nulla commodi.
     </p>
   </div>
 </div>
